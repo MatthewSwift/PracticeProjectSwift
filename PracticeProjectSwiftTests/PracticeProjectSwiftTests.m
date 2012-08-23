e3c7c0faf97b52c9542583434dd41cfcc3e575ed
806-21 <80621@41cs806-21.cooper.edu>
2012-08-23 10:02:11 -0400

    Initial Commit

---
+++ PracticeProjectSwiftTests/PracticeProjectSwiftTests.m
@@ -0,0 +1,32 @@
+//
+//  PracticeProjectSwiftTests.m
+//  PracticeProjectSwiftTests
+//
+//  Created by student on 8/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PracticeProjectSwiftTests.h"
+
+@implementation PracticeProjectSwiftTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PracticeProjectSwiftTests");
+}
+
+@end
